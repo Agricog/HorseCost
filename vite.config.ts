954818508e3e15f5c348567c8c814a47964749b2
Agricog <mick@mickstevenson.com>
2025-11-27 20:47:43 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: 'terser',
   },
-  preview: {
-    port: 4173,
+  server: {
+    port: 5173,
+    open: true,
   },
 })
+
