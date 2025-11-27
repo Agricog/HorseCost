0983ab057f7a1a3fde0ca3388741dda892ac0696
Agricog <mick@mickstevenson.com>
2025-11-27 19:09:26 +0000

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import TermsOfService from './pages/TermsOfService'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-of-service" element={<TermsOfService />} />
+    </Routes>
+  )
+}
+
+export default App
