d5cea4965a6846eb4c8f5b02fd9d82921bb52ca2
Agricog <mick@mickstevenson.com>
2025-11-28 10:54:00 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    middlewareMode: true,
-  },
+    outDir: 'dist'
+  }
 })
 
