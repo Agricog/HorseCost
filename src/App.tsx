956e14096b2e97809f1a31ae45917c580f96f8c6
Agricog <mick@mickstevenson.com>
2025-11-30 16:56:11 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import HorseLiveryCalculatorPage from './pages/HorseLiveryCalculatorPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/calculators/horse-livery" element={<HorseLiveryCalculatorPage />} />
+        <Route path="/horse-livery" element={<HorseLiveryCalculatorPage />} />
       </Routes>
     </Router>
   )
 }
 
 export default App
+
