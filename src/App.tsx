292104c9d6b79e6f408593633c13c9fd2c68220c
Agricog <mick@mickstevenson.com>
2025-12-08 18:12:59 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import Home from './pages/Home'
 import HorseLiveryCalculatorPage from './pages/HorseLiveryCalculatorPage'
 import AnnualHorseCostCalculatorPage from './pages/AnnualHorseCostCalculatorPage'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-        {/* Horse Livery Calculator */}
-        <Route path="/calculators/horse-livery" element={<HorseLiveryCalculatorPage />} />
-        <Route path="/horse-livery" element={<HorseLiveryCalculatorPage />} />
-        
-        {/* Annual Horse Cost Calculator */}
-        <Route path="/annual-horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
-        <Route path="/horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
-        <Route path="/yearly-horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
-      </Routes>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+          {/* Horse Livery Calculator */}
+          <Route path="/calculators/horse-livery" element={<HorseLiveryCalculatorPage />} />
+          <Route path="/horse-livery" element={<HorseLiveryCalculatorPage />} />
+          
+          {/* Annual Horse Cost Calculator */}
+          <Route path="/annual-horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
+          <Route path="/horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
+          <Route path="/yearly-horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
   )
 }
 
