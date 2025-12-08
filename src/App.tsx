a634d29e24fed720ec5bceab2d601f1c8398bb07
Agricog <mick@mickstevenson.com>
2025-12-08 14:48:47 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import HorseLiveryCalculatorPage from './pages/HorseLiveryCalculatorPage'
+import AnnualHorseCostCalculatorPage from './pages/AnnualHorseCostCalculatorPage'
 
 function App() {
   return (
