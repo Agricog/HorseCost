e8f25265398c9e816827cb964482ae2ccd636ec4
Agricog <mick@mickstevenson.com>
2025-12-08 14:47:44 +0000

    Create AnnualHorseCostCalculatorPage.tsx

---
+++ src/pages/AnnualHorseCostCalculatorPage.tsx
@@ -0,0 +1,5 @@
+import AnnualHorseCostCalculator from '../components/AnnualHorseCostCalculator'
+
+export default function AnnualHorseCostCalculatorPage() {
+  return <AnnualHorseCostCalculator />
+}
