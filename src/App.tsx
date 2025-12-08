faf76b9349a854268d971cbac07f83c7e31867bf
Agricog <mick@mickstevenson.com>
2025-12-08 14:50:34 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,15 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        
+        {/* Horse Livery Calculator */}
         <Route path="/calculators/horse-livery" element={<HorseLiveryCalculatorPage />} />
         <Route path="/horse-livery" element={<HorseLiveryCalculatorPage />} />
+        
+        {/* Annual Horse Cost Calculator */}
+        <Route path="/annual-horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
+        <Route path="/horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
+        <Route path="/yearly-horse-cost-calculator" element={<AnnualHorseCostCalculatorPage />} />
       </Routes>
     </Router>
   )
