4ec6f5f8e63598f6323308268e93ce0892d267c8
Agricog <mick@mickstevenson.com>
2025-12-10 06:31:13 +0000

    Update HorseTransportCalculator.tsx

--- src/components/HorseTransportCalculator.tsx
+++ src/components/HorseTransportCalculator.tsx
@@ -6,12 +6,9 @@ import {
   AlertCircle,
   ChevronDown,
   ChevronUp,
-  PoundSterling,
-  CheckCircle2,
   Calendar,
   Star,
-  MapPin,
-  Clock
+  MapPin
 } from 'lucide-react'
 
 export default function HorseTransportCalculator() {
